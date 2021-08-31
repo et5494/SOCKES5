12b6d43604a2e9e33eaf345ac9b3f16c3e5fa73b
et5494 <et5494@163.com>
2021-08-31 13:38:54 +0800

    Create ss5.sh

---
+++ ss5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install -y gcc openldap-devel pam-devel openssl-devel wget net-tools vim
+wget http://jaist.dl.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz
+tar -vzx -f ss5-3.8.9-8.tar.gz
+cd ss5-3.8.9/
+./configure
+make
+make install
+chmod a+x /etc/init.d/ss5
