706de3b2f27fb67d5cec3246a6cff12028d0e452
et5494 <et5494@163.com>
2021-08-31 13:46:27 +0800

    Update ss5.sh

--- ss5.sh
+++ ss5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y gcc openldap-devel pam-devel openssl-devel wget net-tools vim
+yum install -y gcc openldap-devel pam-devel openssl-devel net-tools vim
 wget http://jaist.dl.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz
 tar -vzx -f ss5-3.8.9-8.tar.gz
 cd ss5-3.8.9/
