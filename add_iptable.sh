f420ab8f5f8d46ed9cdd6cae2b13aef45d20174e
et5494 <et5494@163.com>
2021-09-05 15:37:02 +0800

    Update add_iptable.sh

--- add_iptable.sh
+++ add_iptable.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1000 -j MARK --set-mark 1000
-iptables -t nat -A POSTROUTING -m mark --mark 1000 -j SNAT --to-source 198.13.56.218
+iptables -t nat -A POSTROUTING -m mark --mark 1000 -j SNAT --to-source 167.179.68.218
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1001 -j MARK --set-mark 1001
-iptables -t nat -A POSTROUTING -m mark --mark 1001 -j SNAT --to-source 167.179.68.218
+iptables -t nat -A POSTROUTING -m mark --mark 1001 -j SNAT --to-source 45.77.133.195
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1002 -j MARK --set-mark 1002
-iptables -t nat -A POSTROUTING -m mark --mark 1002 -j SNAT --to-source 167.179.79.198
+iptables -t nat -A POSTROUTING -m mark --mark 1002 -j SNAT --to-source 207.148.107.181
 service ss5 stop
-ss5 -u lo1 -b 198.13.56.218
-ss5 -u lo2 -b 167.179.68.218
-ss5 -u lo3 -b 167.179.79.198
+ss5 -u lo1 -b 167.179.68.218
+ss5 -u lo2 -b 45.77.133.195
+ss5 -u lo3 -b 207.148.107.181
 service ss5 start
