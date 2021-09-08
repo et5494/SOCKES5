06a7d9e3b65a23994d2ac3c3a651549141edcb50
et5494 <et5494@163.com>
2021-09-08 23:50:24 +0800

    Update add_iptable_4.sh

--- add_iptable_4.sh
+++ add_iptable_4.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1000 -j MARK --set-mark 1000
-iptables -t nat -A POSTROUTING -m mark --mark 1000 -j SNAT --to-source 45.32.248.50
+iptables -t nat -A POSTROUTING -m mark --mark 1000 -j SNAT --to-source 108.160.134.132
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1001 -j MARK --set-mark 1001
-iptables -t nat -A POSTROUTING -m mark --mark 1001 -j SNAT --to-source 167.179.100.182
+iptables -t nat -A POSTROUTING -m mark --mark 1001 -j SNAT --to-source 45.32.248.50
 iptables -t mangle -A OUTPUT -m owner --uid-owner 1002 -j MARK --set-mark 1002
-iptables -t nat -A POSTROUTING -m mark --mark 1002 -j SNAT --to-source 207.148.103.198
+iptables -t nat -A POSTROUTING -m mark --mark 1002 -j SNAT --to-source 167.179.100.182
 service ss5 stop
-ss5 -u lo1 -b 45.32.248.50
-ss5 -u lo2 -b 167.179.100.182
-ss5 -u lo3 -b 207.148.103.198
+ss5 -u lo1 -b 108.160.134.132
+ss5 -u lo2 -b 45.32.248.50
+ss5 -u lo3 -b 167.179.100.182
 service ss5 start
